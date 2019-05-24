e41a6f1ea65c79fbbd802981a98fa5935e55a270
Valefant <valentino.bernardo_ciddio@smail.th-koeln.de>
2019-05-24 11:57:20 +0200

    :heavy_plus_sign: Add german locale :memo: Improve readability

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
-const currentPath = window.location.pathname;
-const link = document.querySelector(`.navbar-nav a[href='${currentPath}']`);
+const pathname = window.location.pathname;
+const path = pathname.endsWith("/") ? pathname.substr(0, pathname.length - 1) : pathname;
+const link = document.querySelector(`.navbar-nav a[href='${path ? path : "/"}']`);
 link.classList.add("active");
